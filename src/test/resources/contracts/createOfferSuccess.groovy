3c9a636fbe2751accf43da633d671d6aadf8cf33
Gajaharan Satkunanandan <gajaharan.satkunanandan@whitbread.com>
2019-11-02 12:30:20 +0000

    Fixed Contract Verifier Test setup and test.

--- src/test/resources/contracts/createOfferSuccess.groovy
+++ src/test/resources/contracts/createOfferSuccess.groovy
@@ -6,15 +6,15 @@ Contract.make {
     description("Create Offer Request Success")
     request {
         method 'POST'
-        url '/offer'
+        url '/offers'
         body (
                 '''
             {
                 "description": "test 3",
                 "amount": 2.99,
                 "currency": "GBP",
-                "startDate": "2019-10-28 23:45",
-                "endDate": "2019-10-29 23:45"
+                "startDate": "2019-12-28 23:45",
+                "endDate": "2019-12-29 23:45"
             }
                '''
         )
@@ -25,12 +25,8 @@ Contract.make {
 
     response {
         status 201
-        body (
-            '''
-            {
-                "location": "http://localhost:51954/offers/1"
-            }
-            '''
-        )
+        headers {
+            header('location', 'http://localhost/offers/1')
+        }
     }
 }
